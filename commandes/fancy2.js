b389f21728bbfd3d52240874935643ecc7bf4ca6
timnasax <timothymgaya8@gmail.com>
2026-09-13 16:30:07 +0300

    Update fancy2.js

--- commandes/fancy2.js
+++ commandes/fancy2.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 timoth or timnasa\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('TIMNASA', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 fredie tech\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('LUCKY🍀_MD V5', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
